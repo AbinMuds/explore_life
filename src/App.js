9ecc99c46923d2c8706acb2a85019c493dbee9bd
AbinMuds <abinashmudbhari@gmail.com>
2022-04-16 16:39:24 -0600

    adds some Roting functionality to pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./user/pages/Users";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Lets start</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
